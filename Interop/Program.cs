4f8ca92df791ed264c1f60757dde14ebd632eac5
Anand Dyavanapalli <adyavanapalli@gmail.com>
2021-01-28 15:44:55 -0500

    feat: Add interop program for calling into native code.

---
+++ Interop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Interop
+{
+    class Program
+    {
+        [DllImport("../Native/lib/libprime-pi.so", EntryPoint = "prime_pi")]
+        private static extern ulong PrimePi(ulong n);
+
+        static void Main()
+        {
+            ulong n = 1_000_000;
+            Console.WriteLine($"prime_pi({n}): {PrimePi(1_000_000)}");
+        }
+    }
+}
